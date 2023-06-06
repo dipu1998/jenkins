9f6f6c53cad326b2a3b7ca45112121ab78db69ad
dipu1998 <malakardipu1998@gmail.com>
2023-06-06 09:26:58 +0530

    second_comit

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -37,7 +37,7 @@ public class TestGreeter {
 
     @Test
   public void greetShouldIncludeGreetingMessage() {
-    String someone = "World, Thanks";
+    String someone = "hello world";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
